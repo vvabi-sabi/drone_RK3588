c9517abf8abff883fe699c1ef2db07c26b0d984b
BelotserkovskiyVA <v.belotserkovskii@g.nsu.ru>
2023-04-20 21:07:48 +0700

    add pre_process of autoencoder

--- base/pre_process/pre_processes.py
+++ base/pre_process/pre_processes.py
@@ -36,3 +36,9 @@ def pre_unet(frame):
 def pre_resnet(frame):
     NET_SIZE = cfg["inference"]["net_size"]
     return cv2.resize(frame, (NET_SIZE, NET_SIZE))
+
+def pre_autoencoder(frame):
+	NET_SIZE = cfg["inference"]["net_size"] # (320, 160)
+	cx, cy = int(frame.shape[0]/2), int(frame.shape[1]/2)
+    ltrb = cx - NET_SIZE[0]/2, cy - NET_SIZE[1]/2, cx + NET_SIZE[0]/2, cy + NET_SIZE[1]/2
+    return frame[ltrb[0]:ltrb[2], ltrb[1]:ltrb[3]]
