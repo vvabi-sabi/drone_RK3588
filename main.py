0c42dc106b11c42117923716a980382cc4bb506b
BelotserkovskiyVA <v.belotserkovskii@g.nsu.ru>
2023-05-18 17:20:55 +0700

    added base-main-logic

--- main.py
+++ main.py
@@ -5,46 +5,47 @@ from devices import Cam, RK3588
 
 
 def run(device, visualizer, post_process, odometry):
+    device.camera_set()
     device.start()
-    if post_process != None and odometry is None:
+    if post_process is not None and odometry is None:
         while True:
-            frame, outputs = device.get_outputs()
+            frame, outputs = device.get_neuro_outputs()
             frame = post_process(frame, outputs)
             visualizer.show_frame(frame)
-    elif odometry != None and post_process is None:
+    elif odometry is not None and post_process is None:
         while True:
-            _, outputs = device.get_outputs()
+            _, outputs = device.get_neuro_outputs()
             coords = odometry(outputs)
             visualizer.show_trajectory(coords)
-    if post_process != None  and odometry != None:
+    if post_process is not None  and odometry is not None:
         while True:
-            frame, outputs = device.get_outputs()
+            frame, outputs = device.get_neuro_outputs()
             post_process(frame, outputs)
             coords = odometry(outputs)
             visualizer.show_trajectory(coords)
             visualizer.show_frame(frame)
     else:
         while True:
-            outputs = device.get_outputs()
+            _, outputs = device.get_neuro_outputs()
 
 def main(source):
     """Runs inference and addons (if mentions)
     Creating storages and sending data to them
     """
-    q_pre = Queue(maxsize=cfg["inference"]["buf_size"])
+    post_process = True
+    show_trajectory = False
+    queue_size = 5
+    q_pre = Queue(maxsize=queue_size)
     #q_outs = Queue(maxsize=cfg["inference"]["buf_size"])
-    q_post = Queue(maxsize=cfg["inference"]["buf_size"])
+    q_post = Queue(maxsize=queue_size)
     
-    models = ['YOLO'] # 'ResNet', 'UNet', 'Encoder'
-    params = {'post_process': True, 'show_trajectory': False}
-    
-    camera = Cam(source = source,
-                 q_in = q_post,
-                 q_out = q_pre)
+    camera = Cam(source=source,
+                 queue=q_pre)
+    device = RK3588(camera, models, queue=q_post)
+    models = ['YOLO'] # ['ResNet', 'UNet', 'Encoder']
+    post_processes = PostProcesses(models, post_process)
+    odometry_algs = Odometry(models, show_trajectory)
     visualizer = Visualizer()
-    device = RK3588(camera, models)
-    post_processes = PostProcesses(models, params['post_process'])
-    odometry_algs = Odometry(models, params['show_trajectory'])
     try:
         run(device, visualizer, post_processes, odometry_algs)
     except Exception as e:
@@ -53,4 +54,5 @@ def main(source):
 
 
 if __name__ == "__main__":
-    main()
+    camera_source = 11 # 'path/to/video.mp3'
+    main(camera_source)
